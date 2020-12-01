a6f450e6bad419a9a1945822bd5469fd2099ac26
Talal <TalalOthman2000@gmail.com>
2020-12-02 04:06:34 +0800

    added links to each login button

--- SLWA-Talal/resources/views/auth/login.blade.php
+++ SLWA-Talal/resources/views/auth/login.blade.php
@@ -15,9 +15,9 @@
 
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-3">
-                                <a href="" class="btn btn-danger btn-block">Login with Google</a>
-                                <a href="" class="btn btn-primary btn-block">Login with Facebook</a>
-                                <a href="" class="btn btn-dark btn-block">Login with Github</a>
+                            <a href="{{ route('login.google')}}" class="btn btn-danger btn-block">Login with Google</a>
+                            <a href="{{ route('login.facebook')}}" class="btn btn-primary btn-block">Login with Facebook</a>
+                            <a href="{{ route('login.github')}}" class="btn btn-dark btn-block">Login with Github</a>
                             </div>
                         </div>
 
