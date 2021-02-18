007a511e19f4c3cc707246132370ebfd6af986e8
Talal <TalalOthman2000@gmail.com>
2021-02-19 03:01:31 +0800

    fixed minor syntax problem

--- SLWA-Talal/app/Providers/AppServiceProvider.php
+++ SLWA-Talal/app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
-        Scehma::defaultStringLength(191);
+        Schema::defaultStringLength(191);
     }
 
     /**
