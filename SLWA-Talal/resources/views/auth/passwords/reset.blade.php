7055116b5d18d0792df20d7a11e15a2a0196710b
Talal <TalalOthman2000@gmail.com>
2021-02-20 05:38:25 +0800

    fixed minor html problem in reset password page

--- SLWA-Talal/resources/views/auth/passwords/reset.blade.php
+++ SLWA-Talal/resources/views/auth/passwords/reset.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <div class="card-header"><h3>Reset Password</h3></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('password.update') }}">
